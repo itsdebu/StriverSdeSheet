0ff8170c88df989b302694f19b916dc1b4511f44
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-06-07 00:25:18 +0800

    Create best_time_to_buy_and_sell_stock.cpp

---
+++ best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    // Write your code here.
+    // O(n) approach
+    // int mn=prices[0];
+    // int mx=INT_MIN;
+    // for(int i=1;i<prices.size();i++)
+    // {
+    //     if(mn>prices[i])mn=prices[i];
+    //     mx=max(mx,prices[i]-mn);
+    // }return mx;
+}

0f989235a0444d4d55cf65622f13ed2fdcffc448
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-06-12 00:40:52 +0800

    Create DeleteKthNodeFromEndOfLinkedList.cpp

---
+++ DeleteKthNodeFromEndOfLinkedList.cpp
@@ -0,0 +1,53 @@
+Node* removeKthNode(Node* head, int K)
+
+{
+
+    // Write your code here.
+
+    if(K==0)return head;
+
+    Node* first=head;
+
+    Node* second=head;
+
+    Node* ret=head;
+
+    Node* s=head;
+
+    int ss=0;
+
+    while(s!=NULL)
+
+    {
+
+        s=s->next;
+
+        ss++;
+
+    }
+
+    if(K==ss)return head->next;
+
+    for(int i=1;i<=K;i++)
+
+    {
+
+        first=first->next;
+
+    }
+
+    while(first->next!=NULL)
+
+    {
+
+        first=first->next;
+
+        second=second->next;
+
+    }
+
+    second->next=second->next->next;
+
+    return ret;
+
+}

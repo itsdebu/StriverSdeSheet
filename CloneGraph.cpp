bcba73b457d465b505449c53b63162a394367d1d
DIVYANSHU VERMA <91174322+itsdebu@users.noreply.github.com>
2023-06-17 00:56:20 +0800

    Create CloneGraph.cpp

---
+++ CloneGraph.cpp
@@ -0,0 +1,25 @@
+unordered_map<graphNode*,graphNode*>mp;
+graphNode *dfs(graphNode *cur){
+    if(mp.find(cur) != mp.end()){
+        return mp[cur];
+    }
+    graphNode *clone = new graphNode(cur->data);
+    mp[cur] = clone;
+    for(auto it: cur->neighbours){
+        clone->neighbours.push_back(dfs(it));
+    }
+    return clone;
+}
+
+graphNode *cloneGraph(graphNode *node)
+{
+    // Write your code here.
+    graphNode *clone = new graphNode(node->data);
+    if(node == NULL){
+        return NULL;
+    }
+    if(node->neighbours.size() == 0){
+        return clone;
+    }
+    return dfs(node);
+}
